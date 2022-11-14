1fc2401ffe15338e0543b7170794b6ec0e04d571
DarkAngelGiT <dark.angel.wot@gmail.com>
2022-11-14 22:36:49 +0200

    hw 11
    start styles
    start js

---
+++ src/js/getRefs.js
@@ -0,0 +1,7 @@
+// Файл з посиланнями на розмітку
+const refs = {
+    formSearch: document.querySelector('#search-form'),
+    containerDiv: document.querySelector('.gallery'),
+}
+
+export { refs };
